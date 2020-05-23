3e867e2204cff335bd61a283f01873b97ac30246
Babatunde <soft-sky@live.co.uk>
2020-05-23 18:24:54 +0100

    feature[login-page]: Implement login page
    - [x] Create SignInBox
    - [x] Create SignInSection
    - [x] Create SignIn Container
    
    [#]

---
+++ src/components/Section1/SignInBox/SignInBox.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Classes from './SignInBox.module.css';
+import SignInBtn from '../SignIn/SignInBtn';
+
+
+const SignInBox = (props) => {
+    return (
+        
+        <div className={Classes.SignInBox}>
+            <h4>Sign in</h4>
+            <div>
+                <input type="text" name="email" placeholder="Email" id="email" />
+               
+            </div>
+            <div>
+                <input type="password" name="email" placeholder="Password" id="email" />
+
+            </div>
+            <SignInBtn />
+        </div>
+    )
+}
+
+export default SignInBox;
