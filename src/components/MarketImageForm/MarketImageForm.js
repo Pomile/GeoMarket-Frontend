112aa5a4a59d06d7b916722a5276894526345147
Babatunde <soft-sky@live.co.uk>
2020-05-28 09:49:57 +0100

    Refactor styles

--- src/components/MarketImageForm/MarketImageForm.js
+++ src/components/MarketImageForm/MarketImageForm.js
@@ -11,13 +11,13 @@ const MarketImageForm = (props) => {
             <div className={Classes.MarketImageHolder}>
                 <img src={defaultImage} alt={"market"}/>
                 <div className={Classes.MarketImageFormIcon}><img src={removeImage} alt={"removeImage"} /></div>
-                <div className={Classes.MarketImagesHolder}>
-                    <div className={Classes.ImageScroll}>
-                        <div className={Classes.img} > <img src={defaultImage} alt={"market"} /></div>
-                        <div className={Classes.img} > <img src={defaultImage} alt={"market"} /></div>
-                        <div className={Classes.img} > <img src={defaultImage} alt={"market"} /></div>
-                        <div className={Classes.img} > <img src={defaultImage} alt={"market"} /></div>
-                    </div>
+            </div>
+            <div className={Classes.MarketImagesHolder}>
+                <div className={Classes.ImageScroll}>
+                    <div className={Classes.img} > <img src={defaultImage} alt={"market"} /></div>
+                    <div className={Classes.img} > <img src={defaultImage} alt={"market"} /></div>
+                    <div className={Classes.img} > <img src={defaultImage} alt={"market"} /></div>
+                    <div className={Classes.img} > <img src={defaultImage} alt={"market"} /></div>
                 </div>
             </div>
             <center><Button name={"Upload"} style={Classes.Button} clicked={props.upload} /></center>
